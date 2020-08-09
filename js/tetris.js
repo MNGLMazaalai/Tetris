54c548a7f5f098ad597796c83e029be812876ab5
Бат-Эрдэнэ Б <n.baterdene.goodgodking@gmail.com>
2020-08-09 22:12:43 +0800

    added (move destroy rotate add) shapes, gameOver

--- js/tetris.js
+++ js/tetris.js
@@ -2,11 +2,15 @@ const cellSize = 25,
   width = 10,
   height = 20;
 let currentShapePos,
+  oneSave,
+  currentShape,
+  currentShapeData,
+  allShapePos = [],
   gameBoardData = []; // y, x
 
 import { shapes } from './shapes.js';
 // require('pixi-layers');
-let app = new PIXI.Application({
+const app = new PIXI.Application({
   width: width * cellSize,
   height: height * cellSize,
   // backgroundColor: 0xffffff,
@@ -17,7 +21,7 @@ const gameBoard = new PIXI.Container();
 const cellTexture = PIXI.Texture.from('assets/cell.png');
 
 app.stage.addChild(gameBoard);
-// draw game board border
+document.body.onkeydown = checkKey;
 
 // draw game board
 for (let i = 0; i < width * height; i++) {
@@ -41,36 +45,130 @@ function renderSprite(width, height, x, y, texture) {
   sprite.y = y;
   return sprite;
 }
-
 //function for starting game
 function startGame() {
-  let tmp = [];
-  for (let i = 0; i < 10; i++) tmp.push(0);
-  for (let i = 0; i < 20; i++) gameBoardData.push(tmp);
-  // console.log(gameBoardData);
-  let currentShape = dropShape();
-  // shapeFall(currentShape);
+  for (let i = 0; i < height; i++) {
+    let tmp = [];
+    allShapePos.push(new Array(width));
+    for (let i = 0; i < width; i++) tmp.push(0);
+    gameBoardData.push(tmp);
+  }
+
+  currentShape = addShape();
   return setInterval(function () {
-    shapeFall(currentShape);
+    shapeFall();
   }, 1000);
 }
-function shapeFall(currentShape) {
-  console.log(Math.floor(Math.random() * shapes.length));
+// function for checking if it is able to fall
+function checkFall() {
+  for (let i = 0; i < currentShape.length; i++) {
+    let x = currentShape[i].x / cellSize;
+    let y = currentShape[i].y / cellSize;
+    if (y + 1 >= height) return false;
+    if (y + 1 >= 0 && gameBoardData[y + 1][x] == 1) return false;
+  }
+  return true;
+}
+// function for finishing game
+function gameOver() {
+  alert("Game over!!!");
+  clearInterval(game);
+
+}
+//function for clearing line
+function checkDestroy() {
+  for (let i = 0; i < height; i++) {
+    let cnt = 0;
+    for (let j = 0; j < width; j++) {
+      if (gameBoardData[i][j]) cnt++;
+    }
+    if (cnt != 10) continue
+    for (let j = 0; j < 10; j++) {
+      gameBoard.removeChild(allShapePos[i][j]);
+      allShapePos[i][j] = null;
+      gameBoardData[i][j] = 0;
+    }
+    for (let y = i; y > 0; y--) {
+      for (let x = 0; x < width; x++) {
+        if (allShapePos[y - 1][x] != null) {
+          allShapePos[y][x] = allShapePos[y - 1][x];
+          allShapePos[y - 1][x] = null;
+          allShapePos[y][x].y += cellSize;
+        }
+      }
+    }
+    
+    for (let y = i; y > 0; y--) {
+      for (let x = 0; x < width; x++) {
+        if (allShapePos[y][x] != null) {
+          gameBoardData[y][x] = 1;
+        } else {
+          gameBoardData[y][x] = 0;
+        }
+      }
+    }
+  }
+}
+function addCurrentShape(currentShape) {
+  for (let i = 0; i < currentShape.length; i++) {
+    let x = currentShape[i].x / cellSize;
+    let y = currentShape[i].y / cellSize;
+    allShapePos[y][x] = currentShape[i];
+  }
+}
+// drop shape on the gameBoard
+function dropShape() {
+  for (let i = 0; i < currentShape.length; i++) {
+    let x = currentShape[i].x / cellSize;
+    let y = currentShape[i].y / cellSize;
+    if (y < 0 || x < 0) {
+      gameOver();
+      return;
+    }
+    if (y >= 0 && y < height && x >= 0 && x < width) gameBoardData[y][x] = 1;
+  }
+  oneSave = 0;
+  addCurrentShape(currentShape);
+  checkDestroy();
+  currentShape = addShape();
+}
+//make shape fall one grid
+function shapeFall() {
+
+  let curMove = checkFall();
+  if (curMove == false && oneSave == 2) {
+    dropShape(currentShape);
+    return;
+  }
+  if (curMove == false) {
+    oneSave += 1;
+    return;
+  }
   currentShapePos[0] += 1;
   for (let i = 0; i < currentShape.length; i++) {
     currentShape[i].y += cellSize;
   }
 }
-// function for froping shape
-function dropShape() {
+// function for creating shape
+function addShape() {
   let curIdx = Math.floor(Math.random() * shapes.length);
-  let currentShapeData = shapes[curIdx];
+  currentShapeData = shapes[curIdx];
   let len = currentShapeData.length;
-  let currentShape = [];
-  currentShapePos = [-2, 3];
+  let newCurrentShape = [],
+    cnt = 1;
+  currentShapePos = [-1, 3];
+  if (
+    gameBoardData[0][3] == 1 ||
+    gameBoardData[0][4] == 1 ||
+    gameBoardData[0][5] == 1 ||
+    gameBoardData[0][6] == 1
+  )
+    currentShapePos[0] -= 1;
   for (let i = 0; i < len; i++) {
     for (let j = 0; j < len; j++) {
       if (currentShapeData[i][j] == 0) continue;
+      currentShapeData[i][j] = cnt;
+      cnt += 1;
       const currentShapeCell = renderSprite(
         cellSize,
         cellSize,
@@ -78,10 +176,91 @@ function dropShape() {
         (currentShapePos[0] + i) * cellSize,
         PIXI.Texture.from('assets/shapeCell' + curIdx + '.png')
       );
-      currentShape.push(currentShapeCell);
+      newCurrentShape.push(currentShapeCell);
       gameBoard.addChild(currentShapeCell);
     }
   }
-  return currentShape;
-  // gameBoard.addChild(currentShape);
+  return newCurrentShape;
+}
+// function for rotating shapes
+function rotateShape() {
+  let copyData = currentShapeData;
+  let len = currentShapeData.length;
+  for (let i = 0; i < currentShape.length; i++) {
+    let x = currentShape[i].x / cellSize - currentShapePos[1];
+    let y = currentShape[i].y / cellSize - currentShapePos[0];
+    let a = x + currentShapePos[0];
+    let b = len - 1 - y + currentShapePos[1];
+    // if (b < 0 || b >= width) return;
+    if (b < 0) {
+      moveRight();
+    }
+    if (b >= width) {
+      moveLeft();
+    }
+    if (
+      a >= 0 &&
+      b >= 0 &&
+      a < height &&
+      b < width &&
+      gameBoardData[a][b] == 1
+    )
+      return;
+  }
+  for (let i = 0; i < currentShape.length; i++) {
+    let x = currentShape[i].x / cellSize - currentShapePos[1];
+    let y = currentShape[i].y / cellSize - currentShapePos[0];
+    currentShape[i].y = (x + currentShapePos[0]) * cellSize;
+    currentShape[i].x = (len - 1 - y + currentShapePos[1]) * cellSize;
+  }
+}
+function moveLeft() {
+  for (let i = 0; i < currentShape.length; i++) {
+    let x = currentShape[i].x / cellSize;
+    let y = currentShape[i].y / cellSize;
+    if (x - 1 < 0) return;
+    if (y >= 0 && x - 1 >= 0 && gameBoardData[y][x - 1] == 1) return;
+  }
+  currentShapePos[1] -= 1;
+  for (let i = 0; i < currentShape.length; i++) {
+    currentShape[i].x -= cellSize;
+  }
+}
+function moveRight() {
+  for (let i = 0; i < currentShape.length; i++) {
+    let x = currentShape[i].x / cellSize;
+    let y = currentShape[i].y / cellSize;
+    if (x + 1 >= width) return;
+    if (y >= 0 && x + 1 >= 0 && gameBoardData[y][x + 1] == 1) return;
+  }
+  currentShapePos[1] += 1;
+  for (let i = 0; i < currentShape.length; i++) {
+    currentShape[i].x += cellSize;
+  }
+}
+// keyboard event handle
+function checkKey(e) {
+  e = e || window.event;
+  let cur;
+  if (e.keyCode != 32 && (e.keyCode < 37 || e.keyCode > 40)) return;
+  if (e.keyCode == 32) {
+    while (checkFall()) {
+      currentShapePos[0] += 1;
+      for (let i = 0; i < currentShape.length; i++) {
+        currentShape[i].y += cellSize;
+      }
+    }
+    dropShape();
+  }
+  if (e.keyCode == '37') moveLeft();
+  if (e.keyCode == '38') rotateShape();
+  if (e.keyCode == '39') moveRight();
+  if (e.keyCode == '40') {
+    if (checkFall()) {
+      currentShapePos[0] += 1;
+      for (let i = 0; i < currentShape.length; i++) {
+        currentShape[i].y += cellSize;
+      }
+    }
+  }
 }
